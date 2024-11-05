e9c1292a4448c7190fff53969000bca480aee26d
MASUR89 <surmava9@gmail.com>
2024-11-05 15:10:30 +0400

    საბოლოო

---
+++ script.js
@@ -0,0 +1,23 @@
+let a = 15
+let b = a * a
+if (b > 100) {
+    console.log("მეტია ასზე");
+} else {
+    console.log("ნაკლებია ასზე");
+}
+
+let c = 67
+if (c >= 50 && c <= 100 ) {
+    console.log("ნაკლებია 100-ზე");
+} else {
+    console.log("ნაკლებია 50-ზე");
+}
+
+let d = -2
+if (d === 0) {
+    console.log("ტოლია ნულის");
+} else if (d < 0) {
+    console.log("რიცხვი უარყოფითია");
+} else {
+    console.log("რიცხვი მეტია ნულზე");
+}
